57841e3e4c128381e602804776c34c639e62e1de
Ross Byrne <ross.byrne@outlook.ie>
2016-01-16 17:01:56 +0000

    Started Working On Game Background
    Started working on the game's background. It is still a WIP and many many more items will be added to complete the background of the game. Also added a gameManager and enemy script and some folders to the organised.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	/*=========================== Member Variables ===========================*/
+
+	private string type { get; set; }
+	private string enemyName { get; set; }
+	private int health { get; set; }
+	private int speed { get; set; }
+
+
+	/*=========================== Methods ===========================*/
+
+	/*=========================== Awake() ===========================*/
+
+	void Awake(){
+
+
+
+	} // Awake()
+
+} // class
---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	/*=========================== Member Variables ===========================*/
+
+
+	/*=========================== Methods ===========================*/
+
+	/*=========================== Update() ===========================*/
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+} // class
